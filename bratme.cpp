37549c92f82e4624075f7145d912a3e9b0e7aa75
Nikola Petrov <100771659+nikolapP1@users.noreply.github.com>
2025-01-27 23:39:15 +0200

    Add files via upload

---
+++ bratme.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+/*void printarr(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i]<<" ";
+    }
+}
+
+void multiarr(int* arr, int size, int multiplayer) {
+    for (int i = 0; i < size; i++) {
+        arr[i] *= multiplayer;
+    }
+}*/
+
+/*const int MAX_SIZE = 128;
+void find(int arr[][MAX_SIZE], int tofind) {
+    for (int i = 0; i < MAX_SIZE; i++) {
+        for (int j = 0; j < MAX_SIZE; j++) {
+            if (arr[i][j] == tofind) {
+                cout << i << "," << j<<endl;
+                return;
+            }
+        }
+    }
+}
+int main()
+{
+    int matrix[MAX_SIZE][MAX_SIZE];
+    int rows, cols;
+    cin >> rows >> cols;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin>> matrix[i][j] ;
+        }
+    }
+    int number;
+    cin >> number;
+    find(matrix, number);
+    return 0;
+}
+*/
